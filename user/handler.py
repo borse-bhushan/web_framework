276b4cd628b615a1e86e8cb81b263f63b42ff6cd
Bhushan Borse <borsebhushan216@gmail.com>
2025-06-26 18:37:36 +0530

    CRUD for user is added using the framework.

--- user/handler.py
+++ user/handler.py
@@ -21,8 +21,25 @@ def get_user_obj(request: Request, user_id):
         if user["user_id"] == user_id:
             return JsonResponse(data={"users": user})
 
-    return JsonResponse(data={"message": "NO DATA FOUND"})
+    return JsonResponse(data={"message": "User not found"})
 
 
 def get_all_user(request: Request):
     return JsonResponse(data={"data": USERS})
+
+
+def update_user(request: Request, user_id):
+    req_data = request.body
+    for user in USERS:
+        if user["user_id"] == user_id:
+            user.update(req_data)
+            return JsonResponse(data={"message": "User updated", "user": user})
+    return JsonResponse(data={"message": "User not found"})
+
+
+def delete_user(request: Request, user_id):
+    for idx, user in enumerate(USERS):
+        if user["user_id"] == user_id:
+            deleted_user = USERS.pop(idx)
+            return JsonResponse(data={"message": "User deleted", "user": deleted_user})
+    return JsonResponse(data={"message": "User not found"})
